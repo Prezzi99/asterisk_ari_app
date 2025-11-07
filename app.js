e12b2a682f58228f4a4520f6566ba2ebc3d63250
Ishmael Reid <ishmaelreid9@gmail.com>
2025-11-07 18:27:55 -0500

    Initialize HTTP server
    The server uses express to handle routing
    and includes middlewares for file upload
    and handling CORS.

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import fileUpload from 'express-fileupload';
+
+['uncaughtException', 'unhandledRejection'].forEach(event => {
+    process.on(event, (stack) => {
+        console.log(event + '\n', stack);
+    })
+});
+
+const app = express();
+const port = process.env.PORT || 7070;
+
+app.use(cors( {origin: '*'} ));
+
+app.use(fileUpload({
+    abortOnLimit: true, 
+    limits: {
+        fileSize: 16 * 1024 * 1024,
+        files: 1
+    }
+}));
+
+app.listen(port, () => console.log(`Listening on port >> ${port}`));
