c20a15ef721c753f00924c93255fc1cca54bc907
Ishmael Reid <ishmaelreid9@gmail.com>
2025-11-11 11:26:03 -0500

    Update channel record in redis
    The changes herein is used to set the 'answered' and
    'ended' timestamps for a channel record in the cache
    when the respective channel event is received.
    
    These timestamps will be used to determine how much
    a user should be billed based on the call duration.

--- asterisk/socket.js
+++ asterisk/socket.js
@@ -1,5 +1,6 @@
 import { WebSocket } from 'ws';
 import { config } from 'dotenv';
+import { setChannelAnswered, setChannelEnded } from '../redis/utils.js';
 
 config({path: './config.env'});
 
@@ -11,9 +12,24 @@ const ws = new WebSocket(`ws://${host}/ari/events?api_key=${key}&app=${app}`);
 
 ws.on('open', () => console.log('You are connected.'));
 
-ws.on('message', async (message) => {
-    message = JSON.parse(message.toString());
-    console.log(message);
+ws.on('message', async (event) => {
+    event = JSON.parse(event.toString());
+    
+    if (
+        ['ChannelDestroyed', 'ChannelStateChange'].includes(event.type)
+    ) {
+        const { id, state } = event.channel;
+        const timestamp = event.timestamp;
+
+        switch (event.type) {
+        case 'ChannelStateChange':
+            if (state === 'Up') setChannelAnswered(id, timestamp)
+            break
+        case 'ChannelDestroyed':
+            setChannelEnded(id, timestamp);
+            break
+        }
+    }
 });
 
 export default ws;
