4c2281216c8368d0a5fa8feb445216c760267b0e
Ishmael Reid <ishmaelreid9@gmail.com>
2025-11-12 01:15:19 -0500

    Declare auth utils
    hash - This function creates a hash using bcryptjs
    compare - This compares string to a hash and returns
    a boolean result.
    sign - Creates a signed json web token.

--- controllers/utils.js
+++ controllers/utils.js
@@ -1,3 +1,6 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
 export function testRegExp(pairs) {
     for (let item of pairs) {
         const regex = item[0];
@@ -13,4 +16,23 @@ export function testRegExp(pairs) {
 
 export function createContext(user_id, script_id) {
     return `u-${user_id}:s-${script_id}`
+}
+
+export function sign(user_id) {
+    if (!user_id) throw new ReferenceError('Invalid value for token signature');
+
+    return jwt.sign(
+        { user_id },
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.JWT_EXPIRES_IN}
+    );
+}
+
+export async function hash(password) {
+    const salt = await bcrypt.genSalt(14);
+    return await bcrypt.hash(password, salt)
+}
+
+export async function compare(password, hash) {
+    return await bcrypt.compare(password, hash);
 }
