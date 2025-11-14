c2c57e8902ae7f958eaeff3b8c3a682da11c4946
Ishmael Reid <ishmaelreid9@gmail.com>
2025-11-13 19:11:18 -0500

    Add event for call status update
    When the 'call-status' event is emitted, user's
    will receive a message via their websocket connection
    with the status of the call and the callee's phone
    number.

--- events.js
+++ events.js
@@ -1,8 +1,17 @@
 import { EventEmitter } from 'events';
 import { billUser } from './utils.js';
+import { wss } from './app.js'
 
 const emitter = new EventEmitter();
 
 emitter.on('bill-user', billUser);
+
+emitter.on('call-status', (user_id, status, callee) => {
+    wss.clients.forEach(client => {
+        const message = JSON.stringify({status, callee});
+
+        if (client.user == user_id) client.send(message);
+    });
+});
 
 export default emitter;
