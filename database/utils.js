f3be6202486a7d640bc868c378bd1bf5525c59c7
Ishmael Reid <ishmaelreid9@gmail.com>
2025-11-30 09:16:02 -0500

    Modify getCampaignResources
    Instead of choosing a single endpoint to use
    for the campaign based on the caller_id (did)
    the user specified, the application will
    evenly distribute calls among all endpoints.
    Hence, the function no longer needs to fetch
    the endpoint associated with a caller_id.
    
    Likewise, rather than using a single caller id,
    the application will use the round robin method
    to select a caller id for each call.

--- database/utils.js
+++ database/utils.js
@@ -10,20 +10,19 @@ export async function saveSheet(user_id, title, file) {
     );
 }
 
-export async function getCampaignResources(user_id, sheet_id, script_id, caller_id) {
+export async function getCampaignResources(user_id, sheet_id, script_id) {
     const dataset = await pool.query(`
         SELECT content FROM sheets WHERE id = ? AND user_id = ?;
         SELECT id FROM scripts WHERE id = ? AND user_id = ?;
-        SELECT phone_number, internal_name FROM dids INNER JOIN itsps ON
-        itsps.id = dids.itsp_id WHERE user_id = ? AND dids.id = ?;`,
-        [sheet_id, user_id, script_id, user_id, user_id, caller_id]
+        SELECT JSON_ARRAYAGG(phone_number) as caller_ids
+        FROM dids WHERE user_id = ?;`,
+        [sheet_id, user_id, script_id, user_id, user_id]
     );
 
     return {
         sheet: dataset[0][0][0]?.content,
         script_id: dataset[0][1][0]?.id,
-        phone_number: dataset[0][2][0]?.phone_number,
-        endpoint: dataset[0][2][0]?.internal_name
+        caller_ids: dataset[0][2][0]?.caller_ids,
     }
 }
 
